f513cade832867351b65a4e60dbb308728562f44
mariialima <mariiaeduardafl@gmail.com>
2023-05-04 11:41:06 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+function Confirmar ()
+{
+    texto = nome.value // pega o que foi digitado
+    teste.innerHTML = texto // insere no id do span
+}
+
+function MostrarJanela()
+{
+    janelinha.style.display = "block"
+}
+
+function Fechar()
+{
+    janelinha.style.display = "none"
+}
+
+soma = 0
+function Somar()
+{
+    soma = soma + 1
+    resultado.value = soma
+}
+
+function Imprimir()
+{
+    window.print();
+}
